09c7bfedbdb3c66d88cced85277e4b4c5862d98e
irondog888 <irondoggo888@gmail.com>
2025-03-28 16:27:59 -0700

    blooteg implemented

--- example_mods/stages/Blooteg.lua
+++ example_mods/stages/Blooteg.lua
@@ -3,14 +3,16 @@ function onCreate()
 	makeLuaSprite('stage','poops/blooteg/mesa',-550,-300)
 	makeLuaSprite('gorl','poops/blooteg/azucar',-250,0)
 	makeLuaSprite('nut','poops/blooteg/nut',550,400)
-	makeAnimatedLuaSprite('silly','poops/blooteg/Fumos',850,100)
+	makeAnimatedLuaSprite('silly','poops/blooteg/Fumos',850,200)
 	addAnimationByPrefix('silly','bop','dance',24,false)
 
 	scaleObject('bg',1.2,1.2)
 	scaleObject('stage',1.2,1.2)
 	scaleObject('gorl',0.8,0.8)
 	scaleObject('nut',0.35,0.35)
-	scaleObject('silly',0.7,0.7)
+	scaleObject('silly',0.55,0.55)
+
+	setObjectOrder('gfGroup',2)
 
 	setScrollFactor('gfGroup',1,1)
 	setScrollFactor('bg',0.9,0.9)
